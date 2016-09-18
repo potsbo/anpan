1711cd066917c6c775a6f777662f661683424601
Shimpei Otsubo <potsbo@gmail.com>
2016-09-19 03:41:22 +0900

    Simple consonant list method added

--- spec/anpan_spec.rb
+++ spec/anpan_spec.rb
@@ -5,15 +5,23 @@ describe 'My behaviour' do
   let(:consonant) { Consonant.new('c', {'output'=>'k'}) }
   let(:vowel) { Vowel.new('a', 'a') }
 
+  before do
+    anpan.add_consonants(consonant)
+    anpan.add_vowels(vowel)
+  end
+
   describe '#render' do
     let(:render) { anpan.render }
-    before do
-      anpan.add_consonants(consonant)
-      anpan.add_vowels(vowel)
-    end
 
     it 'should renter "ca\tか"' do
       expect(render).to eq "ca\tか"
+    end
+  end
+
+  describe '#consonant_list' do
+    let(:list) { anpan.consonant_list }
+    it 'should have 1 consonant' do
+      expect(list).to be_a Array
     end
   end
 end
