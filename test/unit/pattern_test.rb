f8f98f7be23a547a51c5b9732f34104b19b75f4f
Shimpei Otsubo <potsbo@gmail.com>
2016-09-19 11:20:14 +0900

    Remove useless test

--- test/unit/pattern_test.rb
+++ test/unit/pattern_test.rb
@@ -2,11 +2,6 @@ require 'test/unit'
 require 'pattern'
 
 class PatternTest < Test::Unit::TestCase
-  def test_render_type
-    assert Pattern.new('a','a').render.is_a? String
-    assert_equal "a\tあ", Pattern.new('a','a').render
-  end
-
   def test_make_jp
     assert_not_nil TABLE
     assert_not_nil TABLE[:a]
