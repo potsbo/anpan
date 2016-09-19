f70e133522c53f5182c5e90f2d9ec2812bcac7b6
Shimpei Otsubo <potsbo@gmail.com>
2016-09-19 23:50:41 +0900

    Fix ww

--- lib/anpan/an/google_japanese_input.rb
+++ lib/anpan/an/google_japanese_input.rb
@@ -55,7 +55,7 @@ class Anpan
               "geminated"  => {"r" => "ltur"}
           },
           "w" => {
-              "geminated"  => {"r" => "ltur"}
+              "geminated"  => {"w" => "ltuw"},
           },
           "g" => {
               "contracted" => {
