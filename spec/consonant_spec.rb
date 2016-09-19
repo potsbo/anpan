bdf819644fffe45683142f31f4fd20e679c6ee1e
Shimpei Otsubo <potsbo@gmail.com>
2016-09-19 11:41:15 +0900

    Add patterns test for consonant

--- spec/consonant_spec.rb
+++ spec/consonant_spec.rb
@@ -7,4 +7,21 @@ describe Consonant do
       expect(consonant.input).to eq 'c'
     end
   end
+
+  describe '#patterns' do
+    it 'should return an Array' do
+      expect(consonant.patterns).to be_a Array
+    end
+    context 'when consonant "c" and vowel "a" given' do
+      before do
+        consonant.addVowel(Vowel.new('a','a'))
+      end
+      it 'should return a Pattern' do
+        expect(consonant.patterns.first).to be_a Pattern
+      end
+      it 'should contain "か"' do
+        expect(consonant.patterns.first.output_jp).to eq 'か'
+      end
+    end
+  end
 end
