af2e6979d73d4f452379aafc9da44bf3654fd51a
Shimpei Otsubo <potsbo@gmail.com>
2016-10-01 22:22:21 +0900

    Delete duplicated patterns

--- lib/anpan/an/dvorakjp.rb
+++ lib/anpan/an/dvorakjp.rb
@@ -338,7 +338,7 @@ class Anpan::An
       { input: :';j', output: :"↓"}, { input: :';k', output: :"↑"}, { input: :';h', output: :"←"}, { input: :';l', output: :"→"},
       { input: :';[', output: :"z["}, { input: :';]', output: :"z]"},
       { input: :';.', output: :"…"}, { input: :';,', output: :"‥"},
-      { input: :xn, output: :nn}, { input: :"n'", output: :nn}, { input: :www, output: :w, addition: :ww, as_is: true}
+      { input: :xn, output: :nn}, { input: :www, output: :w, addition: :ww, as_is: true}
     ]
   }
 end
