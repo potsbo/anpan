eb15958c983fd68dca2aaf99c9a77e83c4c1fc29
Shimpei Otsubo <potsbo@gmail.com>
2016-10-01 12:37:36 +0900

    Add n'

--- lib/anpan/an/dvorakjp.rb
+++ lib/anpan/an/dvorakjp.rb
@@ -354,7 +354,7 @@ class Anpan::An
       { input: :",", output: :","}, { input: :'.', output: :"."},
       { input: :';j', output: :"↓"}, { input: :';k', output: :"↑"}, { input: :';h', output: :"←"}, { input: :';l', output: :"→"},
       { input: :';[', output: :"z["}, { input: :';]', output: :"z]"},
-      { input: :xn, output: :nn}, { input: :www, output: :w, addition: :ww, as_is: true}
+      { input: :xn, output: :nn}, { input: :"n'", output: :nn}, { input: :www, output: :w, addition: :ww, as_is: true}
     ]
   }
 end
