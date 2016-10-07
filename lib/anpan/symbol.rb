befe0a482c1b40d5bb192d56b1de6f6b45e9de04
Shimpei Otsubo <potsbo@gmail.com>
2016-10-08 00:24:04 +0900

    Expand class

--- lib/anpan/symbol.rb
+++ lib/anpan/symbol.rb
@@ -1,17 +1,19 @@
-class Anpan::Symbol
-  attr_reader :input, :output
-  def initialize(input, output, addition, as_is=false)
-    @input    = input
-    @output   = output
-    @addition = addition
-    @as_is    = as_is
-  end
+class Anpan
+  class Symbol
+    attr_reader :input, :output
+    def initialize(input, output, addition, as_is=false)
+      @input    = input
+      @output   = output
+      @addition = addition
+      @as_is    = as_is
+    end
 
-  def render
-    pattern.render
-  end
+    def render
+      pattern.render
+    end
 
-  def pattern
-    Pattern.new(@input,@output,@addition, @as_is)
+    def pattern
+      Pattern.new(@input,@output,@addition, @as_is)
+    end
   end
 end
