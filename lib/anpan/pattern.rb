3e432509956deb077864bc7d7a98f19b828a1e66
Shimpei Otsubo <potsbo@gmail.com>
2016-10-08 18:05:39 +0900

    Fix spaces and some

--- lib/anpan/pattern.rb
+++ lib/anpan/pattern.rb
@@ -3,7 +3,7 @@ require 'anpan/pattern/table'
 class Anpan
   class Pattern
     attr_reader :input, :output, :addition
-    def initialize(input, output=input, addition = nil, as_is = false)
+    def initialize(input, output = input, addition = nil, as_is = false)
       @input    = input
       @output   = output
       @addition = addition
@@ -12,11 +12,11 @@ class Anpan
 
     def render
       output = @as_is ? @output : output_jp
-      [@input, output, @addition].join("\t").gsub(/\t+$/,'')
+      [@input, output, @addition].join("\t").gsub(/\t+$/, '')
     end
 
     def to_h
-      { input: @input, output: output_jp, addition: @addition}
+      { input: @input, output: output_jp, addition: @addition }
     end
 
     def output_jp
@@ -28,12 +28,12 @@ class Anpan
       rest = rest.to_s
       (0..rest.length).each do |i|
         sym = rest[0..i].to_sym
-        unless TABLE[sym].nil? then
-          jp, rest = search_table(rest[(i+1)..-1])
-          return TABLE[sym]+jp, rest
+        unless TABLE[sym].nil?
+          jp, rest = search_table(rest[(i + 1)..-1])
+          return TABLE[sym] + jp, rest
         end
       end
-      return "", rest
+      return '', rest
     end
   end
 end
